011feb0bce3a51f32f9508b6255381d5e372fac4
Jovan Yoshioka <jovanyoshioka@gmail.com>
2024-04-09 23:54:55 -0400

    Added web app file structure.

---
+++ components/landing.js
@@ -0,0 +1,21 @@
+// Reference: https://www.freecodecamp.org/news/reusable-html-components-how-to-reuse-a-header-and-footer-on-a-website/
+
+const landingTemplate = document.createElement('template');
+
+// Possible to import text from an HTML file?
+landingTemplate.innerHTML = `
+  <h1>Landing</h1>
+`;
+
+class Landing extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const shadowRoot = this.attachShadow({ mode: 'closed' });
+    shadowRoot.appendChild(landingTemplate.content);
+  }
+}
+
+customElements.define('landing-component', Landing);
