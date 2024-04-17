5f6dd31038a8d1262c22b4224dd825255cc14bda
Jovan Yoshioka <jovanyoshioka@gmail.com>
2024-04-17 13:16:41 -0400

    Implemented suggestions carousel.

--- components/suggestions/suggestions.js
+++ components/suggestions/suggestions.js
@@ -38,35 +38,166 @@ suggestionsTemplate.innerHTML = `
       </div>
     </section>
     <section class="suggestions-results">
-      <div class="result">
-        <img src="assets/atlanta.JPG" />
-        <div class="tint"></div>
-        <h1>Atlanta</h1>
-        <h2>1</h2>
-      </div><div class="result">
-        <img src="assets/knoxville.JPG" />
-        <div class="tint"></div>
-        <h1>Knoxville</h1>
-        <h2>2</h2>
-      </div><div class="result">
-        <img src="assets/seattle.jpg" />
-        <div class="tint"></div>
-        <h1>Seattle</h1>
-        <h2>3</h2>
-      </div><div class="result">
-        <img src="assets/nyc.jpg" />
-        <div class="tint"></div>
-        <h1>New York City</h1>
-        <h2>4</h2>
-      </div><div class="result">
-        <img src="assets/florida.jpg" />
-        <div class="tint"></div>
-        <h1>Florida</h1>
-        <h2>5</h2>
+      <div class="result-group left">
+        <div class="result">
+          <img src="assets/florida.jpg" />
+          <div class="tint"></div>
+          <h1>Placeholder</h1>
+          <h2>#</h2>
+        </div><div class="result">
+          <img src="assets/knoxville.JPG" />
+          <div class="tint"></div>
+          <h1>Placeholder</h1>
+          <h2>#</h2>
+        </div><div class="result">
+          <img src="assets/nyc.jpg" />
+          <div class="tint"></div>
+          <h1>Placeholder</h1>
+          <h2>#</h2>
+        </div><div class="result">
+          <img src="assets/seattle.jpg" />
+          <div class="tint"></div>
+          <h1>Placeholder</h1>
+          <h2>#</h2>
+        </div><div class="result">
+          <img src="assets/atlanta.JPG" />
+          <div class="tint"></div>
+          <h1>Placeholder</h1>
+          <h2>#</h2>
+        </div>
       </div>
+      <div class="result-group right">
+        <div class="result">
+          <img src="assets/seattle.jpg" />
+          <div class="tint"></div>
+          <h1>Placeholder</h1>
+          <h2>#</h2>
+        </div><div class="result">
+          <img src="assets/knoxville.JPG" />
+          <div class="tint"></div>
+          <h1>Placeholder</h1>
+          <h2>#</h2>
+        </div><div class="result">
+          <img src="assets/atlanta.JPG" />
+          <div class="tint"></div>
+          <h1>Placeholder</h1>
+          <h2>#</h2>
+        </div><div class="result">
+          <img src="assets/florida.jpg" />
+          <div class="tint"></div>
+          <h1>Placeholder</h1>
+          <h2>#</h2>
+        </div><div class="result">
+          <img src="assets/nyc.jpg" />
+          <div class="tint"></div>
+          <h1>Placeholder</h1>
+          <h2>#</h2>
+        </div>
+      </div>
+      <div class="result-group middle">
+        <div class="result">
+          <img src="assets/knoxville.JPG" />
+          <div class="tint"></div>
+          <h1>Knoxville</h1>
+          <h2>1</h2>
+        </div><div class="result">
+          <img src="assets/atlanta.JPG" />
+          <div class="tint"></div>
+          <h1>Atlanta</h1>
+          <h2>2</h2>
+        </div><div class="result">
+          <img src="assets/seattle.jpg" />
+          <div class="tint"></div>
+          <h1>Seattle</h1>
+          <h2>3</h2>
+        </div><div class="result">
+          <img src="assets/nyc.jpg" />
+          <div class="tint"></div>
+          <h1>New York City</h1>
+          <h2>4</h2>
+        </div><div class="result">
+          <img src="assets/florida.jpg" />
+          <div class="tint"></div>
+          <h1>Florida</h1>
+          <h2>5</h2>
+        </div>
+      </div>
+      
+      <button class="previous" onclick="showResults(false)">
+        <i class="fa-solid fa-chevron-left"></i>
+      </button>
+      <button class="next" onclick="showResults(true)">
+        <i class="fa-solid fa-chevron-right"></i>
+      </button>
     </section>
   </div>
 `;
+
+/**
+ * Helper function to replace class.
+ * @param node Element node to replace classes.
+ * @param oldClass Class that will be replaced.
+ * @param newClass Class that will be replacing.
+ */
+function replaceClass(node, oldClass, newClass) {
+  node.classList.remove(oldClass);
+  node.classList.add(newClass);
+}
+
+/**
+ * Animates transitioning to next or previous results.
+ * @param next If true, show next results; false, show previous results.
+ */
+function showResults(next) {
+  var doc = document.getElementsByTagName("suggestions-component")[0].shadowRoot;
+
+  // Disable the transition button.
+  var buttonNodes = doc.querySelectorAll("section.suggestions-results button");
+  buttonNodes.forEach((node) => {
+    node.disabled = true;
+  });
+
+  const left = doc.querySelector(".result-group.left");
+  const right = doc.querySelector(".result-group.right");
+  const middle = doc.querySelector(".result-group.middle");
+
+  // Note: CSS attribute adds transition delay to below translation.
+  if (next) {
+    // Slide middle to left, right to middle, and move left to right.
+    middle.style.transform = "translateX(-100%)";
+    right.style.transform = "translateX(0)";
+    left.style.opacity = "0.0";
+    left.style.transform = "translateX(100%)";
+  } else {
+    // Slide middle to right, left to middle, and move right to left.
+    middle.style.transform = "translateX(100%)";
+    left.style.transform = "translateX(0)";
+    right.style.opacity = "0.0";
+    right.style.transform = "translateX(-100%)";
+  }
+  
+  setTimeout(() => {
+    // Update classes to reflect new positions.
+    if (next) {
+      replaceClass(middle, "middle", "left");
+      replaceClass(right, "right", "middle");
+      replaceClass(left, "left", "right");
+
+      left.style.opacity = "1.0";
+    } else {
+      replaceClass(middle, "middle", "right");
+      replaceClass(left, "left", "middle");
+      replaceClass(right, "right", "left");
+
+      right.style.opacity = "1.0";
+    }
+
+    // Re-enable the transition button.
+    buttonNodes.forEach((node) => {
+      node.disabled = false;
+    });
+  }, 750);
+}
 
 class Suggestions extends HTMLElement {
   constructor() {
